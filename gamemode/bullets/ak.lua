b56f2c855625bdb6d8c2b74c7a324024199b41b6
c0bra61 <c0bra@xiatek.org>
2012-08-27 23:53:36 +0100

    +AK Bullets go proper speed

--- gamemode/bullets/ak.lua
+++ gamemode/bullets/ak.lua
@@ -2,7 +2,7 @@
 AKBullet = {}
 
 -- Bullet shit
-AKBullet.Velocity = 24000 -- 2000 feet per second
+AKBullet.Velocity = 2350 * 12 -- 2350 feet per second
 AKBullet.Damage = 35
 AKBullet.GravityModifier = 1
 AKBullet.Name = "AKBullet"
