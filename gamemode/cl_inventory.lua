3d338626ac0d2c4033f99397f73971647576c780
c0bra61 <c0bra@xiatek.org>
2012-08-31 11:19:55 +0100

    +Inventory structure idea

--- gamemode/cl_inventory.lua
+++ gamemode/cl_inventory.lua
@@ -1,4 +1,50 @@
+inv = inv or {}
 
+ExampleItem = {
+	ParentTable = SomeTable,
+	Name = "Example",
+	Info = "An example item",
+	Owner = LocalPlayer(),
+	Actions
+	{
+		["Print"] = function(self) print("Hello", "world") end
+	},
+	Slots = 1,
+	Primary = false,
+	Secondary = false,
+	Draw = function(self, dpanel) /* Draw some fancy stuff... */ end,
+	Drop = function(self) end,
+	PickUp = function(self) end
+	CanPickUp = function(self, pl) end
+}
+
+ExampleInv = {
+	ToolBelt = {
+		Items = {},
+		TotalSpace = 10,
+		FreeSpace = 10
+	}
+	
+	Pockets = {
+		Items = {},
+		TotalSpace = 10,
+		FreeSpace = 10
+	}
+	
+	BackPack = {
+		Items = {},
+		TotalSpace = 24,
+		FreeSpace = 24
+	}
+}
+
+inv.GetInventory = function(pl)
+	if pl == nil then pl = LocalPlayer() end
+	
+	
+end
+
+// GUI Codens below
 if frame then frame:Remove() end
 
 print("HEELO")
@@ -25,23 +71,27 @@ backpack:SetPos(10 + 5 * 2 + SlotSize * 9, 5 + 25)
 backpack:SetSize(SlotSize * 3, SlotSize * 2)
 
 -- Toolbelt
-for i = 0, 10 do
+for i = 0, 10 do -- Only 1 slot size items can go here...
 	local slot = vgui.Create("DPanel", frame)
 	slot:SetPos(10 + i * SlotSize + 5 * i, 5 + 25 + SlotSize * 2 + 5 * 1)
 	slot:SetSize(SlotSize, SlotSize)
 end
 
 -- Generic Inventory
+local slots = 0
+
 for i = 0, 10 do
 	local slot = vgui.Create("DPanel", frame)
 	slot:SetPos(10 + i * SlotSize + 5 * i, 5 + 25 + SlotSize * 3 + 5 * 2 + 5)
 	slot:SetSize(SlotSize, SlotSize)
+	
+	if not slots > 0 then break end
 end
 
 
 -- Backpack
 
-local slots = 16
+slots = 16
 
 local row = 0
 local col = 0
