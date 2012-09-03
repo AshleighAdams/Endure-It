9c3af7e52a609d153edebfa31de621489d86286d
c0bra61 <c0bra@xiatek.org>
2012-09-03 13:45:40 +0100

    ~Decreased bullet speed by 25% (bugfix) ~Fixed awp zeroing

--- gamemode/bullets/buckshot.lua
+++ gamemode/bullets/buckshot.lua
@@ -2,12 +2,12 @@
 BuckShot = BuckShot or {}
 
 -- Bullet shit
-BuckShot.Velocity = 1325 * 12 -- 2000 feet per second
+BuckShot.Velocity = 1325 * 12 * 0.75 -- 2000 feet per second
 BuckShot.Damage = 4
 BuckShot.GravityModifier = 1
 BuckShot.Name = "BuckShot"
 
 BuckShot.Mass = 0.003
-BuckShot.DragCoefficient = 0.35 / (BuckShot.Mass * 1000)
+BuckShot.DragCoefficient = 0.3 / (BuckShot.Mass * 1000)
 
 RegisterBullet(BuckShot)
