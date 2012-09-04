014621b812b7d4e67c43f5d1c06f8c9bbf5748fd
c0bra61 <c0bra@xiatek.org>
2012-09-05 00:10:55 +0100

    Starting the base item entity

---
+++ entities/entities/sent_item_base/init.lua
@@ -0,0 +1,39 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+
+function ENT:SpawnFunction(ply, tr)
+	if (!tr.Hit) then 
+		return
+	end
+	local spawn = tr.HitPos + Vector(0, 0, 15)
+	local ent = ents.Create("sent_item_base")
+	ent:SetPos(spawn)
+	ent:Spawn()
+	return ent
+end
+
+
+function ENT:Initialize()
+	local ent = self.Entity
+	ent:SetModel("models/Combine_turrets/ground_turret.mdl")
+	ent:PhysicsInit(SOLID_VPHYSICS)
+	ent:SetSolid(SOLID_VPHYSICS)
+	ent:SetMoveType(MOVETYPE_VPHYSICS) 
+	ent:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+	local phys = ent:GetPhysicsObject()
+	if phys:IsValid() then
+		phys:Wake()
+	end
+end
+
+function ENT:OnTakeDamage(dmginfo)
+	self.Entity:TakePhysicsDamage(dmginfo)
+end
+
+function ENT:Think()
+	
+end
+
+
---
+++ entities/entities/sent_item_base/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "Base Item"
+ENT.Author			= "victormeriqui & C0BRA"
+ENT.Information		= ""
+ENT.Category		= "Endure It"
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= true
+
