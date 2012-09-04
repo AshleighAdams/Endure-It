3fd3a9187ca81b3ef53478db527614790f2c7017
c0bra61 <c0bra@xiatek.org>
2012-09-04 15:15:46 +0100

    Fixed accuracy of the m4 shotgun

--- gamemode/bullets/buckshot.lua
+++ gamemode/bullets/buckshot.lua
@@ -2,12 +2,13 @@
 BuckShot = BuckShot or {}
 
 -- Bullet shit
-BuckShot.Velocity = 1325 * 12 * 0.75 -- 2000 feet per second
+BuckShot.Velocity = 1525 * 12 * 0.75 -- 2000 feet per second
 BuckShot.Damage = 4
 BuckShot.GravityModifier = 1
 BuckShot.Name = "BuckShot"
+BuckShot.TracerChance = 0
 
-BuckShot.Mass = 0.003
-BuckShot.DragCoefficient = 0.3 / (BuckShot.Mass * 1000)
+BuckShot.Mass = 0.005
+BuckShot.DragCoefficient = 0.25 / (BuckShot.Mass * 1000)
 
 RegisterBullet(BuckShot)
