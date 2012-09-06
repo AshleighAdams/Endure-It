a6e4c34e0637235d1e9d451ff698900da0796872
c0bra61 <c0bra@xiatek.org>
2012-09-06 03:26:30 +0100

    File Downloads

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,10 +1,32 @@
+-- Sound
+resource.AddFile("sound/arma2/bullet_by1.wav")
+resource.AddFile("sound/arma2/bullet_by2.wav")
+resource.AddFile("sound/arma2/bullet_by3.wav")
+resource.AddFile("sound/arma2/bullet_by4.wav")
+resource.AddFile("sound/arma2/bullet_by5.wav")
+resource.AddFile("sound/arma2/bullet_by6.wav")
+resource.AddFile("sound/arma2/sscrack1.wav")
+resource.AddFile("sound/arma2/sscrack2.wav")
+
+-- Models & textures
+resource.AddFile("models/wystan/stanag_magazine.mdl")
+resource.AddFile("models/wystan/ak47magazine.mdl")
+resource.AddFile("models/wystan/m9magazine.mdl")
+resource.AddFile("models/wystan/m249magazine.mdl")
+
+resource.AddFile("materials/wystan/556_normal.vtf")
+resource.AddFile("materials/wystan/AK47/47_mag.vmt")
+resource.AddFile("materials/wystan/AK47/Magazine.vmt")
+resource.AddFile("materials/wystan/M9/frame.vmt")
+resource.AddFile("materials/wystan/M9/frame_normal.vtf")
+resource.AddFile("materials/wystan/M249/boxmag.vmt")
+resource.AddFile("materials/wystan/M249/bullet.vmt")
+
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( 'shared.lua' )
-
-
 include("sv_inventory.lua")
 
 // Serverside only stuff goes here
