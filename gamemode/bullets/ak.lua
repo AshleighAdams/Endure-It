7fa6fe3c6b8de41ffabda6938a30087dafc1c240
c0bra61 <c0bra@xiatek.org>
2012-09-15 21:22:04 +0100

    This bullet doesn't need tracers

--- gamemode/bullets/ak.lua
+++ gamemode/bullets/ak.lua
@@ -6,7 +6,7 @@ AKBullet.Velocity = 2350 * 12 -- 2350 feet per second
 AKBullet.Damage = 35
 AKBullet.GravityModifier = 1
 AKBullet.Name = "AKBullet"
-AKBullet.TracerChance = 1
+AKBullet.TracerChance = 0
 
 AKBullet.Mass = 0.008
 AKBullet.DragCoefficient = 0.265 / (AKBullet.Mass * 1000)
